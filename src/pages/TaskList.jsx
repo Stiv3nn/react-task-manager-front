dcb3f68edef5e8ae26c46f375f602028e2ad70a2
Stiven hoxha <stivenhoxha007@gmail.com>
2025-06-22 09:36:27 +0200

    milestone 12

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,9 +1,20 @@
 // 21 --> VERIFICHIAMO LE NOSTRE CHIAMATE DI USEEFFECT PER LEGGERE LE TASKS E IMPORTIAMO USECONTEXT CHE E' IL NOSTORN HOOK (MILESTONE 2)
-import { useContext, useMemo, useState } from "react";
+import { useCallback, useContext, useMemo, useState } from "react";
 // 22 --> VERIFICHIAMO LE NOSTRE CHIAMATE DI USEEFFECT PER LEGGERE LE TASKS E IMPORTIAMO GLOBALCONTEXT (MILESTONE 2)
 import { GlobalContext } from "../context/GlobalContext";
 // 27 --> IMPORTO TASKROW (MILESTONE 3)
 import TaskRow from "../components/TaskRow";
+
+// 104 --> CREAZIONE DELLA FUNZIONE DI DEBOUNCE PER MIGLIORARE LE PRESTAZIONI (MILESOTNE 12)
+function debounce (callback, delay) {
+    let timer; //SCOPE
+    return (value) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            callback(value);
+        }, delay)
+    }
+}
 
 
 // 5 --> FARE L'EXPORT E LA FUNZIONE DI DEFAULT INIZIALE (MILESTONE 1)
@@ -12,6 +23,11 @@ function TaskList() {
     // 23 --> QUI RACCOGLIAMO LA NOSTRA VALUE CHE AL SUO INTERNO TROVIAMO TASTK E SETTASKS, ANDIAMO A DESTRUTTURARE E VEDERE SE FUNZIONANO (MILESTONE 2)
     const { tasks } = useContext(GlobalContext);
     console.log('Tasks:', tasks);
+
+    // 100 --> CREAZIONE DELLO STATO PER L'INPUT DI RICERCA CHE VERRA' FATTO SULLE TASK (MILESTONE 12)
+    const [searchQuery, setSearchQuery] = useState("");
+    // 105 --> VERSIONE DEBOUNCATA DI SETSEARCHQUEARY (MILESTONE 12)
+    const debounceSetSearchQuery = useCallback(debounce(searchQuery, 500), []);
 
     // 94 --> CREARE I 2 NUOVI STATE (MILESTONE 11)
     const [sortBy, setSortBy] = useState('createdAt');
@@ -31,9 +47,15 @@ function TaskList() {
     }
 
     // 98 --> CREAZIONE DEL USEMEMO (MILESTONE 11)
-    const sortedTask = useMemo(() => {
-        return [...tasks].sort((a, b) => {
+    // 101 --> MODIFICARE L'USEMEMO PER FILTRARE E ORDINARE I TASK (MILESTONE 12)
+    const filteredAndSortedTask = useMemo(() => {
+        // 103 --> PRIMA DI FARE SORT DEVO FARE UN FILTRO (MILESTONE 12)
+        return [...tasks]
+        .filter(t => t.title.toLowerCase().includes(searchQuery.toLowerCase()))
+        .sort((a, b) => {
+
             let comparison;
+
             if (sortBy === 'title') {
                 comparison = a.title.localCompare(b.title)
             } else if (sortBy === 'status') {
@@ -48,11 +70,22 @@ function TaskList() {
             }
             return comparison * sortOrder;
         })
-    }, [tasks, sortBy, sortOrder]);
+        // 102 --> QUESTO USEMEMO DEVE RICEVERE LA NOSTRA SEARCHQUERY (MILESTONE 12)
+    }, [tasks, sortBy, sortOrder, searchQuery]);
 
     return (
         <div>
             <h1>Lista dei task</h1>
+
+            {/* 99 --> CREAZIONE DELL'INPUT PER LA RICERCA DELLE TASK (MILESTONE 12) */}
+            <input
+                type="text"
+                placeholder="Cerca una task..."
+                // value={searchQuery}
+                onChange={e => debounceSetSearchQuery(e.target.value)}
+            />
+
+
             {/* 24 --> STRUTTURARE TASKLIST.JSX COME UNA TABELLA, CON IL NOME, STATO E LA DATA DI CREAZIONE (MILESTONE 3) */}
             <table>
                 <thead>
@@ -75,7 +108,7 @@ function TaskList() {
                 </thead>
                 <tbody>
                     {/* 26 --> FARE UN MAP PER OGNI TASK, PERCHE' PER OGNI TASK DEVO CREARE UN TASKROW A CUI PASSO LA TASK IN QUESTIONE (MILESTONE 3) */}
-                    {sortedTask.map(task => (
+                    {filteredAndSortedTask.map(task => (
                         <TaskRow key={task.id} task={task} />
                     ))}
                 </tbody>
